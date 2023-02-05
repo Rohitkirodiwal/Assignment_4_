8d8de717f3c782f172791cf719632af598d55fe8
Rohitkirodiwal <121967233+Rohitkirodiwal@users.noreply.github.com>
2023-02-05 22:33:52 +0530

    Add files via upload

---
+++ _assignment1.py
@@ -0,0 +1,9 @@
+# Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+# sample input: 10
+
+# sample output: 35
+
+value = lambda ad : ad + 25
+print(value(10))
+
